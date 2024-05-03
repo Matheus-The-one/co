5fa5fe29ad2f97a7bd29d1de99c7c9f399b020bf
matheustheone <matepapava123@gmail.com>
2024-05-03 21:43:07 +0400

    reaq queary devtools ad query

--- app/components/providers/RootProviders.tsx
+++ app/components/providers/RootProviders.tsx
@@ -1,10 +1,14 @@
 "use client"
 import { ThemeProvider } from 'next-themes'
 import React, { ReactNode } from 'react'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 
 const RootProviders = ({children}:{children:ReactNode}) => {
+  const [queryCilent]= React.useState(()=> new QueryClient({}))
   return (
     <div>
+      <QueryClientProvider client={queryCilent}  >
       <ThemeProvider attribute='class'
       defaultTheme='dark'
       enableSystem
@@ -13,6 +17,8 @@ const RootProviders = ({children}:{children:ReactNode}) => {
         {children}
 
       </ThemeProvider>
+      </QueryClientProvider>
+      <ReactQueryDevtools  initialIsOpen={false} />
     </div>
   )
 }
