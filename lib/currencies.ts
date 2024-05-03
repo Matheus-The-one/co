fa31d81138fc49fa5253f3a9439c698f6288e8ee
matheustheone <matepapava123@gmail.com>
2024-05-03 21:08:46 +0400

    adding currencies

---
+++ lib/currencies.ts
@@ -0,0 +1,27 @@
+export const  Currencies=[
+    {
+        value:"USD",
+        label:"$ Dollars",
+        locale:"en-US"
+    },
+    {
+        value:"EUR",
+        label:"€ EURO",
+        locale:"de-DE"
+    },
+    {
+        value:"JPY",
+        label:"¥ Yen",
+        locale:"ja-JP"
+    },
+    {
+        value:"GBP",
+        label:"£ Pound",
+        locale:"en-GB"
+    },
+    {
+        value:"LARI",
+        label:" ₾ Lari",
+        locale:"ge-GE"
+    }
+]
